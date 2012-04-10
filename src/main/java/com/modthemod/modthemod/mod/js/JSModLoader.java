fbf6065504f43bffd432d3b39b638077d1b07304
Ian Macali <ianmacalinao@gmail.com>
2012-04-10 02:06:20 -0500

    Deleted JSDescriptionLoader. Kind of unnecessary.

--- src/main/java/com/modthemod/modthemod/mod/js/JSModLoader.java
+++ src/main/java/com/modthemod/modthemod/mod/js/JSModLoader.java
@@ -17,6 +17,8 @@
 package com.modthemod.modthemod.mod.js;
 
 import com.google.common.base.Charsets;
+import com.modthemod.modthemod.MLogger;
+import com.modthemod.modthemod.mod.Description;
 import org.mozilla.javascript.Callable;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
@@ -24,6 +26,10 @@ import org.mozilla.javascript.ScriptableObject;
 import com.modthemod.modthemod.mod.Mod;
 import com.modthemod.modthemod.mod.ModLoader;
 import com.modthemod.modthemod.mod.ModLanguage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
 
 /**
  * Represents a Javascript mod loader.
@@ -37,7 +43,7 @@ public class JSModLoader implements ModLoader {
         try {
             Context cx = Context.enter();
             Scriptable scope = cx.initStandardObjects();
-            
+
             //Define mod
             scope.put("defineMod", scope, new Callable() {
 
@@ -46,31 +52,30 @@ public class JSModLoader implements ModLoader {
                     if (args.length < 1) {
                         return false;
                     }
-                    
-                    if (!(args[0] instanceof Scriptable)) {
+
+                    if (!( args[0] instanceof Scriptable )) {
                         return false;
                     }
-                    
+
                     Scriptable modData = (Scriptable) args[0];
-                    
+
                     //Name
                     Object nameObj = modData.get("name", modData);
                     if (nameObj == null) {
                         return false;
                     }
                     String name = nameObj.toString();
-                    
+
                     //Description
                     Object descObj = modData.get("description", modData);
-                    if (!(descObj instanceof Scriptable)) {
+                    if (!( descObj instanceof Scriptable )) {
                         return false;
                     }
-                    Scriptable desc = (Scriptable) descObj;
-                    
-                    
-                    
+                    Scriptable descS = (Scriptable) descObj;
+                    Description desc = loadDescription(descS);
+
                     //Parts
-                    
+
                     return null;
                 }
 
@@ -78,7 +83,8 @@ public class JSModLoader implements ModLoader {
 
             //Evaluate
             cx.evaluateString(scope, js, "<mod>", 1, null);
-        } finally {
+        }
+        finally {
             Context.exit();
         }
 
@@ -88,6 +94,72 @@ public class JSModLoader implements ModLoader {
     @Override
     public final ModLanguage getType() {
         return ModLanguage.JAVASCRIPT;
+    }
+
+    /**
+     * Loads the {@link Description} from its {@link Scriptable} object.
+     *
+     * @param descriptionObject The {@link Scriptable} to load.
+     * @return The {@link Description} loaded.
+     */
+    public Description loadDescription(Scriptable descriptionObject) {
+        if (!( descriptionObject instanceof Map )) {
+            MLogger.log(Level.WARNING, "Invalid description to parse for a mod! (Not a Javascript object!)");
+            return null;
+        }
+        Map<String, Object> data = (Map<String, Object>) descriptionObject;
+
+        String description = "Default description.";
+        try {
+            description = (String) data.get("description");
+        }
+        catch (NullPointerException ex) {
+        }
+        catch (ClassCastException ex) {
+            MLogger.log(Level.WARNING, "The field \'description\' in a mod was not a String.", ex);
+        }
+
+        String version = "Unspecified";
+        try {
+            version = (String) data.get("version");
+        }
+        catch (NullPointerException ex) {
+        }
+        catch (ClassCastException ex) {
+            MLogger.log(Level.WARNING, "The field \'version\' in a mod was not a String.", ex);
+        }
+
+        String author = "Unknown";
+        try {
+            author = (String) data.get("author");
+        }
+        catch (NullPointerException ex) {
+        }
+        catch (ClassCastException ex) {
+            MLogger.log(Level.FINE, "The field \'author\' in a mod was not a String.", ex);
+        }
+
+        List<String> authors = new ArrayList<String>();
+        try {
+            authors = (List<String>) data.get("authors");
+        }
+        catch (NullPointerException ex) {
+        }
+        catch (ClassCastException ex) {
+            MLogger.log(Level.FINE, "The field \'authors\' in a mod was not a String.", ex);
+        }
+
+        String url = "Unspecified";
+        try {
+            url = (String) data.get("url");
+        }
+        catch (NullPointerException ex) {
+        }
+        catch (ClassCastException ex) {
+            MLogger.log(Level.FINE, "The field \'url\' in a mod was not a String.", ex);
+        }
+
+        return new Description(description, version, author, authors, url);
     }
 
 }
