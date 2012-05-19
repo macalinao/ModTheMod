edb496330ea7f6f41519b5c44137b4bd723fa1f9
Ian Macalinao <ianmacalinao@gmail.com>
2012-05-19 17:17:02 -0500

    Redo of everything.

--- src/main/java/com/modthemod/modthemod/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * The main package of the ModTheMod modding framework.
- * <p>
- * This package contains the API and the main core
- * of the modding framework.
- * </p>
- */
-package com.modthemod.modthemod;
