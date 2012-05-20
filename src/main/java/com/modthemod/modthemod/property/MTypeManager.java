70f35e44036624462d667c024ddbbfeb2441c10f
Ian Macalinao <ianmacalinao@gmail.com>
2012-05-19 19:19:42 -0500

    Implemented the TypeManager.

--- src/main/java/com/modthemod/modthemod/property/MTypeManager.java
+++ src/main/java/com/modthemod/modthemod/property/MTypeManager.java
@@ -1,6 +1,11 @@
 package com.modthemod.modthemod.property;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+
 import com.modthemod.api.base.Base;
+import com.modthemod.api.event.EventType;
 import com.modthemod.api.mod.Mod;
 import com.modthemod.api.property.Matcher;
 import com.modthemod.api.property.Primitive;
@@ -10,6 +15,12 @@ import com.modthemod.modthemod.MGame;
 
 public class MTypeManager implements TypeManager {
 	private final MGame game;
+
+	private Map<String, Primitive<?>> primitives = new HashMap<String, Primitive<?>>();
+
+	private Map<Mod, Map<String, Base>> bases = new HashMap<Mod, Map<String, Base>>();
+
+	private Map<String, Base> baseNames = new HashMap<String, Base>();
 
 	public MTypeManager(MGame game) {
 		this.game = game;
@@ -29,14 +40,37 @@ public class MTypeManager implements TypeManager {
 
 	@Override
 	public void registerPrimitive(Primitive<?> primitive) {
-		// TODO Auto-generated method stub
-
+		primitives.put(primitive.getName(), primitive);
 	}
 
 	@Override
 	public void registerBase(Base base, Mod mod) {
-		// TODO Auto-generated method stub
+		Map<String, Base> map = getBaseMap(mod);
+		Base exists = map.get(base.getName());
+		if (exists != null) {
+			game.getLogger().log(
+					Level.WARNING,
+					"Base '" + exists.getName()
+							+ "' already exists! Be careful!");
+		}
+		map.put(base.getName(), base);
 
+		// Now the name
+		baseNames.put(mod.getName() + ":" + base.getName(), base);
 	}
 
+	/**
+	 * Gets the map of bases of the given mod.
+	 * 
+	 * @param mod
+	 * @return
+	 */
+	private Map<String, Base> getBaseMap(Mod mod) {
+		Map<String, Base> map = bases.get(mod);
+		if (map == null) {
+			map = new HashMap<String, Base>();
+			bases.put(mod, map);
+		}
+		return map;
+	}
 }
